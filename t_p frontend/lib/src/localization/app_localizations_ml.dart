0812f1f34aa249715353bb6c99c070f4c7c302e1
Muhammed Fahad T <mhdfhdt@gmail.com>
2026-06-19 11:11:51 +0530

    Generated malayalam localization file

---
+++ t_p frontend/lib/src/localization/app_localizations_ml.dart
@@ -0,0 +1,67 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Malayalam (`ml`).
+class AppLocalizationsMl extends AppLocalizations {
+  AppLocalizationsMl([String locale = 'ml']) : super(locale);
+
+  @override
+  String get appTitle => 'ട്രാഫിക് പട്രോൾ';
+
+  @override
+  String get home => 'ഹോം';
+
+  @override
+  String get camera => 'ക്യാമറ';
+
+  @override
+  String get upload => 'അപ്‌ലോഡ്';
+
+  @override
+  String get login => 'ലോഗിൻ';
+
+  @override
+  String get settings => 'ക്രമീകരണങ്ങൾ';
+
+  @override
+  String get theme => 'തീം';
+
+  @override
+  String get language => 'ഭാഷ';
+
+  @override
+  String get light => 'ലൈറ്റ്';
+
+  @override
+  String get dark => 'ഡാർക്ക്';
+
+  @override
+  String get system => 'സിസ്റ്റം';
+
+  @override
+  String get english => 'ഇംഗ്ലീഷ്';
+
+  @override
+  String get loginFailed => 'ലോഗിൻ പരാജയപ്പെട്ടു';
+
+  @override
+  String get ok => 'ശരി';
+
+  @override
+  String get usernamePasswordRequired => 'ഉപയോക്തൃനാമവും പാസ്‌വേഡും ആവശ്യമാണ്.';
+
+  @override
+  String get invalidCredentials => 'തെറ്റായ ഉപയോക്തൃനാമം അല്ലെങ്കിൽ പാസ്‌വേഡ്.';
+
+  @override
+  String get username => 'ഉപയോക്തൃനാമം';
+
+  @override
+  String get password => 'പാസ്‌വേഡ്';
+
+  @override
+  String get dontHaveAccountSignUp => 'അക്കൗണ്ട് ഇല്ലേ? സൈൻ അപ്പ് ചെയ്യുക';
+}
